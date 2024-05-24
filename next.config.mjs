b9b918bef917384340e2b0d0e93ce26723733f93
SayanTan772 <sayantandey506@gmail.com>
2024-05-25 00:06:02 +0530

    added comments

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+// This is used to store a configuration object in nextjs declared with environment variables
+// The environment variable is a way of storing sensitive information like database credentials in our case, 
+// API keys, and other configuration details
+// In our case, the `env` is the environment variable which stores MYSQL database configuration details
 const nextConfig = {
     env: {
-        'MYSQL_HOST': '127.0.0.1',
-        'MYSQL_PORT': '3306',
-        'MYSQL_DATABASE': 'demo',
-        'MYSQL_USER': 'root',
-        'MYSQL_PASSWORD': '',
+        'MYSQL_HOST': '127.0.0.1', // This property specifies the hostname or IP address of the MySQL server. In our case the XAMPP Localhost
+        'MYSQL_PORT': '3306', // You can get this port number from the XAMPP control panel beside mysql option
+        'MYSQL_DATABASE': 'demo', // Name of the Database
+        'MYSQL_USER': 'root', // This specifies the username used to authenticate the access to database
+        'MYSQL_PASSWORD': '', // Keep the password empty to easily access your database
     }
 }
 
-export default nextConfig;
+export default nextConfig; // Export this configuration object to be used later on as module to establish connectivity to MYSQL database
+
+// These configurations can be utilited by using process.env keyword ( more on them in db.js file )
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,10 @@
-import mysql from "mysql2/promise";
+import mysql from "mysql2/promise"; // Importing module to interact with sql database
 
+// query: The SQL query string to be executed
+// values: An array of values to be used with the query for parameterized queries
 export async function query({ query, values = [] }) {
 
+  // Establish Database connection by using the evn variable
   const dbconnection = await mysql.createConnection({
     host: process.env.MYSQL_HOST,
     post: process.env.MYSQL_PORT,
@@ -9,12 +12,16 @@ export async function query({ query, values = [] }) {
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
   });
+  // This section just deals with the utilization of the env configuration values
 
   try {
-    const [results] = await dbconnection.execute(query, values);
-    dbconnection.end();
-    return results;
+    const [results] = await dbconnection.execute(query, values); // Executing the passed query ( with or without values )
+    dbconnection.end(); // End DB Connection after query is executed successfully
+    return results; // Returns the result of the query as defined in the endpoint 
   } catch (error) {
-    throw Error(error.message);
+    throw Error(error.message); // Throw a new error in case query was not executed successfully
   }
 }
+
+// Import query function from this db.js file in every api route (endpoint) that you create
+// and pass the custom query and values that you require into this function for execution
